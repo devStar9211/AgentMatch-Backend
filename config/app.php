bba523c379ca366c33ea29ec48942e01d5d56601
Kazunobu000 <high_aim@outlook.com>
2018-10-30 02:51:14 +0900

    Update config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'agentMatch'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://agentmatch.jp/AgentMatch-Backend'),
 
     /*
     |--------------------------------------------------------------------------
