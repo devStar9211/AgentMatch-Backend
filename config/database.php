5b54ae8a6701cb7b701b18aa670122688e687f40
Kazunobu000 <high_aim@outlook.com>
2018-10-30 11:43:25 +0900

    Update config/database.php

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+// define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+// define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+// define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+// define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
 return [
 
     /*
@@ -45,17 +45,17 @@ return [
         'mysql' => [
             'driver' => 'mysql',
 
-            'host' => RDS_HOSTNAME,
-            'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
-            
-            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => RDS_HOSTNAME,
             // 'port' => env('DB_PORT', '3306'),
-            // 'database' => env('DB_DATABASE', 'agent_match'),
-            // 'username' => env('DB_USERNAME', 'root'),
-            // 'password' => env('DB_PASSWORD', ''),
+            // 'database' => RDS_DB_NAME,
+            // 'username' => RDS_USERNAME,
+            // 'password' => RDS_PASSWORD,
+            
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'agent_match'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
