b7b3252c35c382142f098216ee7fe1b0bbe4ce12
Kazunobu000 <high_aim@outlook.com>
2018-11-07 11:52:29 +0900

    Update app/Thank.php

---
+++ app/Thank.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace agent_match;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thank extends Model
+{
+    //
+  protected $fillable = [
+    'user_id', 'target_id', 'comment'
+  ];
+
+  function thanks_senders(int $id) {
+    return $this -> where('target_id', $id) -> get();
+  }
+}
